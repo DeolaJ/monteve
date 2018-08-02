87875ec130627cf66805b9a15ac60e104c6f08e2
Adeola Adeyemo <adejoe97@yahoo.com>
2018-08-02 22:39:43 +0100

    Change in port

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-var express = require('express'); 
-var path = require('path'); 
-var app = express();
+const express = require('express'); 
+const path = require('path'); 
+const app = express();
+var port = process.env.PORT;
 
-var Ravepay = require('ravepay');
-var rave = new Ravepay('FLWPUBK-b60faa242c217210eb53400fcf4f0d97-X', 'FLWSECK-13dc0b9fad6017ca16bd0d16d306e049-X', true);
+const Ravepay = require('ravepay');
+const rave = new Ravepay('FLWPUBK-b60faa242c217210eb53400fcf4f0d97-X', 'FLWSECK-13dc0b9fad6017ca16bd0d16d306e049-X', true);
 
 app.use('/', express.static(path.join(__dirname, 'public')));
-
 
 app.get('/' , (req , res) => {
     res.redirect('/index.html'); 
 });
-app.listen(3000);
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
